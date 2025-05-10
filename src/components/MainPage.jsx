93227acd48aba9bdea3400721473274c511e04e1
mat1520 <azaazay09@gmail.com>
2025-05-10 11:51:29 -0500

    Revertir el botón de menú móvil en el header para usar el botón original con FaBars y toggleMobileMenu

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -120,24 +120,8 @@ const MainPage = () => {
                 <li><Link to="/contacto">Contacto</Link></li>
               </ul>
             </nav>
-            <button
-              className="lg:hidden text-white hover:text-gray-300 focus:outline-none"
-              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
+            <button className="mobile-menu-btn" onClick={toggleMobileMenu}>
+              <FaBars />
             </button>
           </div>
         </header>
